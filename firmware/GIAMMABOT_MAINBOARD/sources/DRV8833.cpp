f5b103a0f9e58413374a5282f8989646f14f834c
Marco Giammarini <m.giammarini@warcomeb.it>
2021-12-18 08:50:04 +0100

    Started working on motor control

---
+++ firmware/GIAMMABOT_MAINBOARD/sources/DRV8833.cpp
@@ -0,0 +1,52 @@
+/*
+ * GIAMMABOT - MAIN BOARD
+ * Copyright (C) 2021 GIAMMATeam <http://www.warcomeb.it>
+ *
+ * Authors:
+ *  Marco Giammarini <m.giammarini@warcomeb.it>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/*!
+ * \file DRV8833.cpp
+ *
+ * \brief
+ *
+ * \copyright This file is part of GIAMMABOT Project
+ *            Copyright (C) 2021 GIAMMATeam <http://www.warcomeb.it>
+ */
+
+#include <DRV8833.h>
+
+DRV8833::DRV8833(PWMChannel a1, PWMChannel a2, PWMChannel b1, PWMChannel b2):
+    mChannelA1(a1),
+    mChannelA2(a2),
+    mChannelB1(b1),
+    mChannelB2(b2)
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+DRV8833::~DRV8833()
+{
+    // TODO Auto-generated destructor stub
+}
+
---
+++ firmware/GIAMMABOT_MAINBOARD/sources/DRV8833.h
@@ -0,0 +1,57 @@
+/*
+ * GIAMMABOT - MAIN BOARD
+ * Copyright (C) 2021 GIAMMATeam <http://www.warcomeb.it>
+ *
+ * Authors:
+ *  Marco Giammarini <m.giammarini@warcomeb.it>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/*!
+ * \file DRV8833.h
+ *
+ * \brief
+ *
+ * \copyright This file is part of GIAMMABOT Project
+ *            Copyright (C) 2021 GIAMMATeam <http://www.warcomeb.it>
+ */
+
+#ifndef DRV8833_H_
+#define DRV8833_H_
+
+#include "libohiboard.h"
+
+#include "PWMChannel.h"
+
+class DRV8833
+{
+public:
+    DRV8833(PWMChannel a1, PWMChannel a2, PWMChannel b1, PWMChannel b2);
+    virtual ~DRV8833();
+
+private:
+
+    PWMChannel mChannelA1;
+    PWMChannel mChannelA2;
+    PWMChannel mChannelB1;
+    PWMChannel mChannelB2;
+};
+
+#endif /* DRV8833_H_ */
